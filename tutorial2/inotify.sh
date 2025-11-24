fc9c19755ee0bfc4ef19fcfbc4ac7bb2e27e7b8f
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-11-23 23:53:11 -0500

    new commit

---
+++ tutorial2/inotify.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+sudo mkdir -p /temp/NewFolder
+inotifywait -m /temp/NewFolder
+
--- tutorial2/prof-menus.sh
+++ tutorial2/prof-menus.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-
+: '
 select car in BMW MERCEDES TESLA ROVER TOYOTA Exit
 do
     # echo "You have selected $car"
@@ -24,7 +24,7 @@ do
         echo "ERROR! Please selected between 1..5";;
     esac
 done
-
+'
 
 
 # echo "press any key to continue"
@@ -54,3 +54,17 @@ done
 
 echo "Script continues after the select loop."
 '
+
+echo "press any key to continue"
+
+while [ true ]
+do
+    read -t 3 -n 1
+if [ $? = 0 ]
+then
+    echo "you have terminated the script"
+    exit;
+else
+    echo "waiting for you to press the key sir!"
+fi
+done
