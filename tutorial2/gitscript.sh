c42acc8ec8bd494611a5c0f8021cbabc2fa6e845
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-11-24 00:00:59 -0500

    inotify

--- tutorial2/gitscript.sh
+++ tutorial2/gitscript.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 \"Your commit message\""
+    exit 1
+fi
+
+COMMIT_MESSAGE="$1"
+
 git add .
-git commit -m "new commit"
-git push origin main
+git commit -m "$COMMIT_MESSAGE"
+git push
+echo "Changes pushed to GitHub successfully!"
 
