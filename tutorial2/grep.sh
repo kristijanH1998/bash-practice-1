efba9a4930563a2e5773a2a77b0a12732f374013
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-12-06 22:42:20 -0500

    some grep examples

---
+++ tutorial2/grep.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+echo "Enter filename to search text from"
+read fileName
+
+if [[ -f $fileName ]]
+then
+    echo "Enter the text to search"
+    read grepvar
+    grep -i -n $grepvar $fileName
+    echo "The matching lines are: "
+    grep -i -n $grepvar $fileName | grep -o '[0-9]\+' | tr '\n' ', ' | sed 's/,$//'
+    cnt=$(grep -i -c $grepvar $fileName)
+    printf "\n"
+    echo "$grepvar occurs $cnt times in $fileName"
+else
+    echo "$fileName does not exist"
+fi
+
